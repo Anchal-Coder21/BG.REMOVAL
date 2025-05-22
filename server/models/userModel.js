0e266bf9185c4358e2fead2f4ef2043a8fd37898
Anchal-Coder21 <anchalsingh8886@gmail.com>
2025-05-22 19:36:53 +0530

    Webhooks Added

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,14 +1,15 @@
-import mongoose from"mongoose";
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    clerkId: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
-    photo: { type: String, required: true },
-    firstname: { type: String },
-    lastname: { type: String },
-    creditBalance: { type: Number, default:5 }
-})
+  clerkId: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  photo: { type: String, required: true },
+  firstname: { type: String },
+  lastname: { type: String },
+  creditBalance: { type: Number, default: 5 }
+});
 
-const userModel =  mongoose.model.user || mongoose.model("user,userSchema")
+//  Use mongoose.models to prevent overwrite during hot-reloading (e.g. in Next.js)
+const User = mongoose.models.User || mongoose.model("User", userSchema);
 
-export default userModel;
+export default User;
